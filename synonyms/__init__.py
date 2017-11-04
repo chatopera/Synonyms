de9c7ec7485aab4ed071099a1a2a90be9f6af2ad
huyingxi <531196781@qq.com>
2017-11-04 10:45:56 -0500

    Update __init__.py
    updata sentence similarity's threshold

--- synonyms/__init__.py
+++ synonyms/__init__.py
@@ -192,7 +192,8 @@ def _similarity_distance(s1, s2):
     # https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.linalg.norm.html
     g = 1 / (np.linalg.norm(a - b) + 1)
     u = _unigram_overlap(s1, s2)
-    r = g * 0.8 + u * 0.2
+    r = g * 1.4 + u * 0.2
+    r = min((r * 10 + 0.1) , 1.0)
 
     return float("%.3f" % r)
 
