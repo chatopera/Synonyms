e73f2a0dd315be25d0735ff434991e3c372b979d
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-12-31 19:01:05 +0800

    Refine distance params, upgrade to v2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ Welcome
 
 setup(
     name='synonyms',
-    version='1.9',
+    version='2.0',
     description='Chinese Synonyms for Natural Language Processing and Understanding',
     long_description=LONGDOC,
     author='Hai Liang Wang, Hu Ying Xi',
