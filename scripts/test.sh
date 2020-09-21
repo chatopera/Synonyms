1fdfaaea404a914eea8053d0e7447171d9420ea5
Hai Liang Wang <hain@chatopera.com>
2020-09-21 13:19:15 +0800

    Ignore synonyms/data/words.vector

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+export PYTHONUNBUFFERED=1
+export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+cd $baseDir/..
+if [ -f .env ]; then
+    echo "load env with" `pwd`"/.env"
+    source .env
+fi
+
+python demo.py
